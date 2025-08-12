ba005d22b11cbdc69ae5b20f3af83872785acbe5
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-08-13 00:02:42 +0530

    nested comments starter code

---
+++ 4-nested-comments/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function NestedComments() {}
+
+function App() {
+  return (
+    <div>
+      <h1>Nested Comments</h1>
+      <NestedComments />
+    </div>
+  );
+}
+
+export default App;
